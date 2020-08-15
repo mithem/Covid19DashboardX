bde9d5f98f8ed5b5297f2c68f56653831f109757
mithem <mithem@github.com>
2020-08-15 12:47:53 +0200

    Initial Commit

---
+++ Shared/Covid19DashboardXApp.swift
@@ -0,0 +1,17 @@
+//
+//  Covid19DashboardXApp.swift
+//  Shared
+//
+//  Created by Miguel Themann on 15.08.20.
+//
+
+import SwiftUI
+
+@main
+struct Covid19DashboardXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
